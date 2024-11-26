c812d49179f9486f9c16aec3eac8493c78b4dbda
Samuel Suarez <157648233+SSUAREZD@users.noreply.github.com>
2024-11-25 21:42:35 -0500

    Add files via upload

---
+++ TallerSwing/Taller Swing Restaurantes Esqueleto/src/uniandes/dpoo/swing/interfaz/principal/PanelBotones.java
@@ -0,0 +1,57 @@
+package uniandes.dpoo.swing.interfaz.principal;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelBotones extends JPanel implements ActionListener
+{
+    /**
+     * El comando para el botón para crear un nuevo restaurante
+     */
+    private static final String NUEVO = "nuevo";
+
+    /**
+     * El comando para el botón para ver todos los restaurantes en el mapa
+     */
+    private static final String VER = "ver";
+
+    private JButton butNuevo;
+    private JButton butVerTodos;
+    private VentanaPrincipal ventanaPrincipal;
+
+    public PanelBotones( VentanaPrincipal ventanaPrincipal )
+    {
+        this.ventanaPrincipal = ventanaPrincipal;
+
+        setLayout( new FlowLayout( ) );
+
+        // Agrega el botón para crear un nuevo restaurante
+        // TODO completar
+        butNuevo = new JButton(NUEVO);
+        add(butNuevo);
+        // Agrega el botón para ver todos los restaurantes
+        // TODO completar
+        butVerTodos = new JButton(VER);
+        add(butVerTodos);
+        // Agrega el botón 
+    }
+
+    @Override
+    public void actionPerformed( ActionEvent e )
+    {
+        String comando = e.getActionCommand( );
+        if( comando.equals( NUEVO ) )
+        {
+            ventanaPrincipal.mostrarVetanaNuevoRestaurante( );
+        }
+        else if( comando.equals( VER ) )
+        {
+            ventanaPrincipal.mostrarVentanaMapa( );
+        }
+    }
+}
